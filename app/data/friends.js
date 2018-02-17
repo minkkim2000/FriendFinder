5450a8526c7f7a0a3acf514248e1209fe38838ff
minkkim2000 <minkkim2000@gmail.com>
2018-02-17 01:29:28 -0800

    works

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+// 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
+
+
+var friends = [
+{
+  "name":"Archer",
+  "photo":"https://images.complex.com/complex/image/upload/c_limit,w_680/fl_lossy,pg_1,q_auto/damesu7uen6wcznc1p9l.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+  "name":"Tobias",
+  "photo":"https://flavorwire.files.wordpress.com/2013/05/tobias1.jpg?w=1056",
+  "scores":[
+      3,
+      4,
+      1,
+      1,
+      2,
+      3,
+      1,
+      2,
+      3,
+      5
+    ]
+}
+];
+
+module.exports = friends;
